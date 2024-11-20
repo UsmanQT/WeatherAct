27e628d1a5c1df8380bb4378c1a1a5a09d384701
usmanqt <qureshiu@mail.gvsu.edu>
2024-11-20 17:02:37 -0500

    finding specific weather for a place using lat and long on a new route

--- Program.cs
+++ Program.cs
@@ -22,11 +22,33 @@ List<WeatherDto> weatherInfo = [
         TimeOnly.FromDateTime(DateTimeOffset.FromUnixTimeSeconds(1732106522).DateTime),
         TimeOnly.FromDateTime(DateTimeOffset.FromUnixTimeSeconds(1732140910).DateTime),
         "Grand Rapids"
-    )
+    ),
+    new (
+        5391959,
+        37.7669m,
+        -122.4321m,
+        "Clouds",
+        "overcast clouds",
+        288.1m,
+        287.61m,
+        286.25m,
+        289.71m,
+        75m,
+        1016m,
+        8.49m,
+        "US",
+        TimeOnly.FromDateTime(DateTimeOffset.FromUnixTimeSeconds(1732114563).DateTime),
+        TimeOnly.FromDateTime(DateTimeOffset.FromUnixTimeSeconds(1732150519).DateTime),
+        "San Francisco"
+)
 ];
 
-app.MapGet("/weather/GR", () => weatherInfo);
+app.MapGet("/weather", () => weatherInfo);
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/weather/{lat}/{lng}", (decimal lat, decimal lng) => {
+    WeatherDto? weatherData = weatherInfo.Find(weatherInfo => weatherInfo.Lat == lat && weatherInfo.Long == lng);
+
+    return weatherData is null ? Results.NotFound() : Results.Ok(weatherData);
+});
 
 app.Run(); 
